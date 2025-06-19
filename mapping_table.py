16fe4d031bb8e6086b57b403099e583a14e6f81c
ANKIT <158843621+ANKIT0017@users.noreply.github.com>
2025-06-19 21:23:56 +0530

    Add files via upload

---
+++ mapping_table.py
@@ -0,0 +1,14 @@
+class_mapping = {
+    'Throwing': 0,
+    'Running': 1,
+    'Sitting': 2,
+    'Standing': 3,
+    'Walking': 4,
+    'Clapping': 5,
+    'Stretching': 6,
+    'Kicking': 7,
+    'Lifting': 8,
+    'Dumping': 9,
+    'Turning': 10,
+    'Pointing': 11
+}
